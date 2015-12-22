5184d8cefeac50d914a0b73b5a57c432dcfde1c4
Athimont <athimont1@hotmail.fr>
2015-12-22 13:55:03 +0100

    création de 2 classe

---
+++ Tweet.py
@@ -0,0 +1,9 @@
+class Tweet:
+	""" Classe qui défini un tweet par :
+	- son texte
+	- sa date de publication"""
+
+	def __init__(self, texte, date):
+		self.text = texte
+		self.date = date
+
---
+++ User.py
@@ -0,0 +1,28 @@
+import Tweet
+
+class User:
+	""" Classe définisant un utilisateur caractérisé par:
+	- son Identifiant
+	- son Mot de passe
+	- son Nom
+	- son prénom"""
+	def __init__(self,identifiant,password,nom,prenom):
+		self.id = identifiant
+		self.password = password
+		self.nom = nom
+		self.prenom = prenom
+
+	def publie (text):
+		
+	def abonne (user):
+
+	def desabonne (user):	
+
+	def affichecompte ():
+
+	def afficheabonnement ():
+
+	def connecte ():
+
+	def deconnecte ():
+
